459abcc0f4ab4f24b0f7dd9a32b7998c3934aa30
jihan-kim99 <jinnkenny99@gmail.com>
2024-07-02 09:14:39 +0900

    init

---
+++ app/api/askAI/route.ts
@@ -0,0 +1,41 @@
+import OpenAI from "openai";
+import { NextRequest, NextResponse } from "next/server";
+
+const streamToString = async (stream: any): Promise<string> => {
+  const chunks = [];
+  for await (const chunk of stream) {
+    chunks.push(chunk);
+  }
+  return Buffer.concat(chunks).toString("utf8");
+};
+
+export async function POST(req: NextRequest) {
+  const apiKey = process.env.OPENAI_API_KEY;
+  const body = await streamToString(req.body);
+  const prompt = JSON.parse(body).prompt;
+  const openai = new OpenAI({
+    apiKey: apiKey,
+    dangerouslyAllowBrowser: true,
+  });
+  const completion = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    response_format: { type: "json_object" },
+    messages: [
+      {
+        role: "system",
+        content: `
+            You are a japaense tutor who will listen to the user and provide feedback on their japanese.
+            If they did not do very well you are allow to be mad and give fierce feedback to them.
+            You can keep asking questions until the user is satisfied with their performance.
+            Make the user feel like they are in a real japanese class.
+            Keep ask the questions so the user can practice their japanese.
+            User can write Romaji or Hiragana and you will respond in Hiragana with (translation in input language).
+            return the answer in json format.
+          `,
+      },
+      { role: "user", content: prompt },
+    ],
+  });
+
+  return NextResponse.json({ text: completion.choices[0] }, { status: 200 });
+}
